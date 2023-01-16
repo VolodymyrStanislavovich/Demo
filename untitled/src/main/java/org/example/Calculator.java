56af3b4ce1314003ecaf0f9c8890dba8eda209d0
VolodymyrStanislavovich <103888469+VolodymyrStanislavovich@users.noreply.github.com>
2023-01-16 16:48:43 +0200

    leson 4

--- untitled/src/main/java/org/example/Calculator.java
+++ untitled/src/main/java/org/example/Calculator.java
@@ -1,12 +1,24 @@
 package org.example;
 
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
 public class Calculator {
     int sum(int ... values) {
-        int sum = 0;
-        for(int x: values){
-            sum += x;
+        System.out.println(" int");
+        int result = 0;
+        for(int i : values){
+            result = result + i;
         }
-        return sum;
+        return result;
+    }
+    float sum (float... values){
+        System.out.println(" float");
+            float result = 0f;
+            for(float i : values){
+                result = result + i;
+            }
+            return result;
+
     }
 
 }
--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -5,16 +5,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Calculator calculator =new Calculator();
-        System.out.println(calculator.sum(2,2));
-        System.out.println(calculator.sum(2,2,6));
+        Scanner scanner = new Scanner(System.in);
+        Calculator calculator = new Calculator();
 
-//      Cat cat = new Cat();
-//      cat.name = "Barsik";
-//      cat.say();
-//      cat.say("Meou");
-
-
+        System.out.println(calculator.sum(2, 8, 8));
+        System.out.println(calculator.sum(2.4f, 8.5f, 8.5f));
     }
 }
 
